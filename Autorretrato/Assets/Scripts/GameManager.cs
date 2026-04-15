1c2b529f818e19510f1fe84d8305fd6a5142c3cd
eire44 <eireduo44@gmail.com>
2026-04-15 09:15:26 -0300

    energy bar

--- Autorretrato/Assets/Scripts/GameManager.cs
+++ Autorretrato/Assets/Scripts/GameManager.cs
@@ -1,17 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     int levelIndex = 0;
     int completeTasksIndex = 0;
+    int tasksLeft = 0;
     public GameObject winScreen;
+    public Image energyFiller;
     public List<Levels_Controller> levels = new List<Levels_Controller>();
     // Start is called before the first frame update
     void Start()
     {
-        
+        tasksLeft = levels[levelIndex].tasksAmount;
     }
 
     // Update is called once per frame
@@ -23,6 +26,7 @@ public class GameManager : MonoBehaviour
     public void taskCompleted()
     {
         completeTasksIndex++;
+        tasksLeft--;
         if(completeTasksIndex >= levels[levelIndex].tasksAmount) 
         {
             winScreen.SetActive(true);
@@ -38,5 +42,20 @@ public class GameManager : MonoBehaviour
             bubble.gameObject.SetActive(true);
             t.taskObject.layer = LayerMask.NameToLayer("Interactive Objects");
         }
+    }
+
+    public void reduceEnergy(bool taskCompleted)
+    {
+        float reductionPerTask = 1f / levels[levelIndex].tasksAmount;
+        if (taskCompleted)
+        {
+            //energyFiller.fillAmount = levels[levelIndex].tasksAmount / tasksLeft;}
+            energyFiller.fillAmount = reductionPerTask;
+        }
+        else
+        {
+            energyFiller.fillAmount = reductionPerTask / 3; ;
+        }
+        
     }
 }
