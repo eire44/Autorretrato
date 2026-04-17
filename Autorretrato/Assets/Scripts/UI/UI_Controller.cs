378362bc4e498f0cf3e803cf60a9fbb7e402b2ae
eire44 <eireduo44@gmail.com>
2026-04-17 18:08:53 -0300

    room UI

--- Autorretrato/Assets/Scripts/UI/UI_Controller.cs
+++ Autorretrato/Assets/Scripts/UI/UI_Controller.cs
@@ -7,6 +7,8 @@ public class UI_Controller : MonoBehaviour
 {
     public GameObject endGameScreen;
     public GameObject HUD;
+    public GameObject volumeScreen;
+    //public AudioSource audioSource_Click;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,6 +21,23 @@ public class UI_Controller : MonoBehaviour
         
     }
 
+    public void openVolumeManager()
+    {
+        //audioSource_Click.Play();
+        if (volumeScreen.activeInHierarchy)
+        {
+            volumeScreen.SetActive(false);
+            HUD.SetActive(true);
+            Time.timeScale = 1f;
+        }
+        else
+        {
+            volumeScreen.SetActive(true);
+            HUD.SetActive(false);
+            Time.timeScale = 0f;
+        }
+    }
+
     public void showEndGameScreen()
     {
         endGameScreen.SetActive(true);
@@ -28,7 +47,24 @@ public class UI_Controller : MonoBehaviour
 
     public void backToMainMenu()
     {
+        //audioSource_Click.Play();
         Time.timeScale = 1f;
         SceneManager.LoadScene("MainMenu");
+    }
+
+    public void Jugar()
+    {
+        //audioSource_Click.Play();
+        SceneManager.LoadScene("Room");
+    }
+
+    public void Salir()
+    {
+        //audioSource_Click.Play();
+        #if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+        #else
+                                            Application.Quit();
+        #endif
     }
 }
