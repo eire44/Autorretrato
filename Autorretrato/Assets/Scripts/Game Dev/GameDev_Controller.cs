3e41486f1e2536d52032c096db55d3e28bd9f472
eire44 <eireduo44@gmail.com>
2026-07-09 22:09:16 -0300

    game dev task

---
+++ Autorretrato/Assets/Scripts/Game Dev/GameDev_Controller.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameDev_Controller : PuzzleManager
+{
+    public List<FurnitureToPlace> furniturePairs = new List<FurnitureToPlace>();
+    bool playAudioOnce = false;
+    public GameObject taskUI;
+    
+    void Update()
+    {
+        if(taskUI.activeInHierarchy)
+        {
+            if (checkFurnitureInPlace())
+            {
+                if (!playAudioOnce)
+                {
+                    playAudioOnce = true;
+                    activateFeedback(true, feedbackBubble);
+                }
+            }
+            else
+            {
+                playAudioOnce = false;
+                activateFeedback(false, feedbackBubble);
+            }
+        }
+        
+    }
+
+    bool checkFurnitureInPlace()
+    {
+        foreach (FurnitureToPlace FtP in furniturePairs)
+        {
+            DraggableObject dO = FtP.draggableObject.GetComponent<DraggableObject>();
+            if (dO.target != FtP.dropZone.gameObject)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
